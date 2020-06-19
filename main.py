7f8f1db2ac66361d92765b0459738e61c952cd66
Joseph Vossler <joseph.vossler1@gmail.com>
2020-06-19 17:48:22 +0000

    Started the bag of words python script to count different words in an article

---
+++ main.py
@@ -0,0 +1,67 @@
+# https://stackabuse.com/python-for-nlp-creating-bag-of-words-model-from-scratch/
+
+import nltk
+import numpy as np
+import random
+import string
+import bs4 as bs
+import urllib.request
+import re
+import lxml.html.soupparser
+import nltk
+nltk.download('punkt')
+
+raw_html = urllib.request.urlopen('https://en.wikipedia.org/wiki/Natural_language_processing')
+raw_html = raw_html.read()
+
+article_html = bs.BeautifulSoup(raw_html, 'lxml')
+
+article_paragraphs = article_html.find_all('p')
+
+article_text = ''
+
+for para in article_paragraphs:
+  article_text += para.text
+
+corpus = nltk.sent_tokenize(article_text)
+
+for i in range(len(corpus )):
+  corpus [i] = corpus [i].lower()
+  corpus [i] = re.sub(r'\W',' ',corpus [i])
+  corpus [i] = re.sub(r'\s+',' ',corpus [i])
+print(len(corpus))
+print(corpus[30])
+
+
+
+#def main():
+#   filepath = sys.argv[1]
+#
+#   if not os.path.isfile(filepath):
+#       print("File path {} does not exist. Exiting...".format(filepath))
+#       sys.exit()
+#  
+#   bag_of_words = {}
+#   with open(filepath) as fp:
+#       cnt = 0
+#       for line in fp:
+#           print("line {} contents {}".format(cnt, line))
+#           record_word_cnt(line.strip().split(' '), bag_of_words)
+#           cnt += 1
+#   sorted_words = order_bag_of_words(bag_of_words, desc=True)
+#   print("Most frequent 10 words {}".format(sorted_words[:10]))
+#  
+#def order_bag_of_words(bag_of_words, desc=False):
+#   words = [(word, cnt) for word, cnt in bag_of_words.items()]
+#   return sorted(words, key=lambda x: x[1], reverse=desc)
+#
+#def record_word_cnt(words, bag_of_words):
+#    for word in words:
+#        if word != '':
+#            if word.lower() in bag_of_words:
+#                bag_of_words[word.lower()] += 1
+#            else:
+#                bag_of_words[word.lower()] = 1
+#
+#if __name__ == '__main__':
+#    main()
